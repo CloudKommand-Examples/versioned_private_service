879aa660b1d6c8085dfe208f2778fe9dc58befed
Azra Eiden <carter.eiden@gmail.com>
2023-09-20 03:38:44 -0400

    Begun

---
+++ api/api_lambda/lambda_function.py
@@ -0,0 +1,58 @@
+import boto3
+import os
+import json
+import traceback
+from mangum import Mangum
+
+from fastapi import FastAPI, APIRouter, Depends, Request, HTTPException, Header, status, Body, Path, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi.security.api_key import APIKeyHeader
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from starlette.exceptions import ExceptionMiddleware
+
+from aws_lambda_powertools import Logger
+
+
+
+logger = Logger(service="whiplash-api")
+
+app = FastAPI(
+    title="Whiplash API",
+    summary="CloudKommand-deployed standalone API that provides an easy-to-use api interface for self-hosted Whiplash, a serverless vector store.",
+    description="""
+The fundamental units in Whiplash are collections and items, where an item is a vector, and a collection is a set of items that are indexed for fast search. 
+
+Default settings for collections are set during deployment, but all settings can be changed when creating a collection.
+    """,
+    docs_url="/docs",
+    root_path="/live",
+    version=os.environ.get("VERSION", "1.0.0")
+)
+app.add_middleware(ExceptionMiddleware, handlers=app.exception_handlers)
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
+
+project_router = APIRouter()
+
+@logger.inject_lambda_context(log_event=True)
+def lambda_handler(event, context):
+    return Mangum(app, api_gateway_base_path="/live")(event, context)
+
+@app.exception_handler(Exception)
+async def unhandled_exception_handler(request, err):
+    logger.exception(f"Unhandled exception in {request.url.path}: {err}")
+    return JSONResponse(status_code=500, content={"detail": "Internal Server Error"})
+
+@app.get("/version", tags=["Version"])
+async def version() -> str:
+    return "V1"
+
+
+# @app.post("/initialize", tags=["Initialize"])
+# def initialize() -> CreateSuccess:
+#     return api_initialize()
+
+app.include_router(project_router, prefix="/projects")
+
